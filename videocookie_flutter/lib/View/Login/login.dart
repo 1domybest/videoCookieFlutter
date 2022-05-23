887ac6fec59bbff63cccc5b43c561eff1e912b7a
editsay <editsay.official@gmail.com>
2022-05-23 18:30:37 +0900

    2-2
    2-2

--- videocookie_flutter/lib/View/Login/login.dart
+++ videocookie_flutter/lib/View/Login/login.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:videocookie_flutter/helper/login_background.dart';
 import 'package:http/http.dart' as http;
--- videocookie_flutter/lib/main.dart
+++ videocookie_flutter/lib/main.dart
@@ -3,7 +3,23 @@ import 'package:flutter/material.dart';
 import 'View/Login/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        iconTheme: IconThemeData(
+          color: Color.fromRGBO(130, 66, 34, 1)
+        ),
+        appBarTheme: AppBarTheme(
+          color: Color.fromRGBO(130, 66, 34, 1)
+        ),
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.red)
+        )
+      ),
+      home: List(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,6 +30,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'videocookie',
       home: AuthPage()
+    );
+  }
+}
+
+
+class List extends StatelessWidget {
+  const List({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('비디오쿠키'),
+      ),
+      endDrawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(130, 66, 34, 1),
+              ),
+              child: Text('온석태 님'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Text('ㅎㅇ'),
     );
   }
 }
